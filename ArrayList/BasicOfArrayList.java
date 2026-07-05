8160c33e2c3adb85d3db7962c89405d01e8e6a42
Tulsi Keswani <169170759+TulsiKeswani@users.noreply.github.com>
2026-07-05 18:22:26 +0000

    Solved Basic Problems on ArrayList

--- ArrayList/BasicOfArrayList.java
+++ ArrayList/BasicOfArrayList.java
@@ -1,5 +1,7 @@
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.Arrays;
+import java.util.Collections;
 
 public class BasicOfArrayList{
     public static void main(String args[]){
@@ -53,5 +55,54 @@ public class BasicOfArrayList{
         }
 
         System.out.println();
+
+        // Print Reverse ArrayList
+
+        for(int i = list.size() -1;  i>=0; i--){
+            System.out.print(list.get(i) + " ");
+        }
+
+        System.out.println();
+
+        // Find Maximum IN ArrayList
+
+        ArrayList<Integer> l = new ArrayList<>();
+
+        l.add(10);
+        l.add(3);
+        l.add(140);
+        l.add(1);
+        l.add(7);
+
+        int maximum = Integer.MIN_VALUE;
+        for(int i = 0; i < l.size(); i++){
+            maximum = Math.max(maximum,l.get(i));
+        }
+
+        System.out.println("Maximum Value is : " + maximum);
+
+        // Swap 2 Numbers in ArrayList
+
+            int idx1 = 1;
+            int idx2 = 3;
+
+            l.clear();
+            l.addAll(Arrays.asList(2,5,9,3,6));
+
+            System.out.println("Before Swap --> " + l);
+            int temp = l.get(idx1);
+            l.set(idx1,l.get(idx2));
+            l.set(idx2,temp);
+
+            System.out.println("After Swap --> " + l);
+
+            // Sorting ArrayList --> Ascending Order
+
+            Collections.sort(l);
+            System.out.println(l);
+
+            // Sorting ArrayList --> Decending order
+            Collections.sort(l,Collections.reverseOrder());
+            System.out.println(l);
     }
 }
